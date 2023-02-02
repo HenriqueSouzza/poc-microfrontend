71dafddfe7369f26375ca73dbcbbbf8e9a071f45
Henrique Souza <henrique.souza@newfold.com>
2023-02-02 16:17:14 -0300

    feat: adding worker to microfrontend manager

---
+++ worker/src/index.ts
@@ -0,0 +1,37 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `wrangler dev src/index.ts` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export interface Env {
+	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+	// MY_KV_NAMESPACE: KVNamespace;
+	//
+	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+	// MY_DURABLE_OBJECT: DurableObjectNamespace;
+	//
+	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+	// MY_BUCKET: R2Bucket;
+}
+
+export default {
+	async fetch(request) {
+
+		const urlTemp = new URL(request.url)
+
+		let url = `http://localhost:8082`;
+
+		if (urlTemp.pathname == '/sites') {
+			url = 'http://cliente.whmcs.brasil:5101/'
+		}
+
+		request = new Request(url, request);
+		const response = await fetch(request);
+		return new Response(response.body, response);
+  },
+};
